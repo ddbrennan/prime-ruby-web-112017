d79f4bd4559e261472dce936678072a8046daf6c
David Brennan <ddbren@gmail.com>
2017-11-05 17:28:04 -0500

    prime? function working

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  return false if num < 2
+  range = (2...num).to_a
+  prime = true
+  range.each do |factor|
+    if num % factor == 0
+      prime = false
+    end
+  end
+  prime
+end
